eb29d39ad1c9d939cdcad304412282f07a6f0457
Windy <lauren.euge@gmail.com>
2020-09-13 15:14:43 -0700

    tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -10,13 +10,26 @@ router.get('/', (req, res) => {
     include:[{
       model:Product,
       through:ProductTag
-    }]
-  }).then(tags =>{
-    res.json(tags)
-  })
+    },
+  ],
+  }).then((tags) => res.status(200).json(tags))
+    .catch ((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model:Product,
+        through: ProductTag,
+      },
+    ],
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
