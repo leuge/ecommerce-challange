070ac04e9babc8d7b159628877e91d2f53357df5
Windy <lauren.euge@gmail.com>
2020-09-13 15:32:24 -0700

    final tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,14 +35,30 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.sttatus(404).json(err));
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // delete on tag by its `id` value
 });
 
